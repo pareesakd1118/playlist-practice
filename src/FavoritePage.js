2ccd2b60d7c624e4ab04fb23ed7e414eac4bf06a
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-08-22 22:06:03 -0700

    Initial commit

---
+++ src/FavoritePage.js
@@ -0,0 +1,7 @@
+
+export default function FavoritePage() {
+
+    return (
+        <div>fav page</div>
+    )
+}
---
+++ src/FirstPage.js
@@ -0,0 +1,7 @@
+
+export default function FirstPage() {
+
+    return (
+        <div>First Page</div>
+    )
+}
